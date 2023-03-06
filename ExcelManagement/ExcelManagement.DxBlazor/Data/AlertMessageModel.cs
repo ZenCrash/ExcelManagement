c2cb60f3457c0685c5f15ad9835a98b9b8fcc222
PieckaAndrej <10407548@ucn.dk>
2023-03-06 22:15:53 +0100

    Alert message

---
+++ ExcelManagement/ExcelManagement.DxBlazor/Data/AlertMessageModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ExcelManagement.DxBlazor.Data
+{
+    public class AlertMessageModel
+    {
+        public string Message { get; set; }
+        public Guid Guid { get; }
+
+        public AlertMessageModel(string message) 
+        {
+            Message = message;
+            Guid = Guid.NewGuid();
+        }
+    }
+}
