d4a9004028087e53414dce3364b5e767208457cb
Allan <76095439+ZenCrash@users.noreply.github.com>
2023-04-25 08:53:37 +0200

    class lobary and test updated

--- ExcelManagement/ExcelManagement.ClassLibary/ExcelRepository.cs
+++ ExcelManagement/ExcelManagement.ClassLibary/ExcelRepository.cs
@@ -32,6 +32,30 @@ namespace ExcelManagement.ClassLibary
 
         }
 
+        //HeaderListHelper
+        public List<string> IncrementDuplicateStringInListString(List<string> inputList)
+        {
+            Dictionary<string, int> stringCounts = new Dictionary<string, int>();
+            List<string> outputList = new List<string>();
+
+            foreach (string str in inputList)
+            {
+                if (stringCounts.ContainsKey(str))
+                {
+                    int count = stringCounts[str];
+                    stringCounts[str] = count + 1;
+                    outputList.Add(str + count.ToString());
+                }
+                else
+                {
+                    stringCounts.Add(str, 1);
+                    outputList.Add(str);
+                }
+            }
+
+            return outputList;
+        }
+
         //---------------------------------------------------------------------------//
         /* GetAll                                                                    */
         //---------------------------------------------------------------------------//
@@ -51,7 +75,9 @@ namespace ExcelManagement.ClassLibary
 
             foreach (var worksheet in workbook.Worksheets)
             {
-                //IF Sheet empty header or data, add new data to header row, and save it.
+
+                //IF Sheet first row (heder row) has no data. add new data to header row, and save it.
+                //A sheet MUST have a header row
                 if (worksheet.FirstRow().CellsUsed().Count() == 0)
                 {
                     var maxCols = 4; // If the sheet doesn't contain any rows, add data to header.
@@ -64,13 +90,26 @@ namespace ExcelManagement.ClassLibary
                     workbook.SaveAs(fullFilePath);
                 }
 
-                // If the sheet contains data
-                var headers = worksheet.RowsUsed().First();
-                var dataRows = worksheet.RowsUsed().Skip(1);
-                var sheetData = new List<dynamic>();
+                //if Duplicate Names in header then indent the name.
+                List<string> headersWithoutSpacesList = new();
+                for (int i = 0; i < worksheet.RowsUsed().First().Cells().Count(); i++)
+                {
+                    string headerWithoutSpaceses = worksheet.RowsUsed().First().Cell(i + 1).Value.ToString().Replace(" ", "");
+                    headersWithoutSpacesList.Add(headerWithoutSpaceses);
+                }
+                headersWithoutSpacesList = IncrementDuplicateStringInListString(headersWithoutSpacesList);
 
+                //If Sheet has Header and data
                 if (worksheet.RowsUsed().Skip(1).Count() != 0)
                 {
+                    //here we assume that header row and atleast 1 other row has data.
+                    var headers = worksheet.RowsUsed().First();
+                    var dataRows = worksheet.RowsUsed().Skip(1);
+                    var sheetData = new List<dynamic>();
+
+
+
+
                     foreach (var dataRow in dataRows)
                     {
                         dynamic item = new ExpandoObject();
@@ -80,36 +119,37 @@ namespace ExcelManagement.ClassLibary
                         for (int i = 0; i < headers.Cells().Count(); i++)
                         {
                             var xlCellView = new XlCellView(dataRow.Cell(i + 1));
-                            dictionary[headers.Cell(i + 1).Value.ToString().Replace(" ", "")] = xlCellView;
+                            dictionary[headersWithoutSpacesList.ElementAt(i)] = xlCellView;
                         }
 
                         sheetData.Add(item);
                     }
-
+                    data[worksheet.Name] = sheetData;
                 }
-
-                else //if header colum has data, but rest dossent. then select first 4 rows
+                else // if sheet dossent have data
                 {
-                    int maxRows = 2; //how many empty rows sould be fetched
+                    int maxEmptyRowsToDisplay = 2;
+                    var headers = worksheet.RowsUsed().First();
+                    var sheetData = new List<dynamic>();
 
-                    for (int row = 2; row <= maxRows + 1; row++)
+                    for (int rowIndex = 2; rowIndex < maxEmptyRowsToDisplay + 2; rowIndex++)
                     {
                         dynamic item = new ExpandoObject();
                         var dictionary = (IDictionary<string, object>)item;
 
-                        dictionary["GridId"] = row;
-
-                        for (int i = 0; i < headers.Cells().Count(); i++) //Cells assign placeholder values
+                        dictionary["GridId"] = rowIndex;
+                        for (int columnIndex = 1; columnIndex < headers.Cells().Count() + 1; columnIndex++)
                         {
-                            var xlCellView = new XlCellView(worksheet.Cell(row, i + 1));
-                            dictionary[headers.Cell(i + 1).Value.ToString().Replace(" ", "")] = xlCellView;
+                            var xlCellView = new XlCellView(worksheet.Cell(rowIndex, columnIndex));
+                            dictionary[headersWithoutSpacesList.ElementAt(columnIndex - 1)] = xlCellView;
                         }
 
                         sheetData.Add(item);
                     }
+                    data[worksheet.Name] = sheetData;
                 }
 
-                data[worksheet.Name] = sheetData;
+
             }
 
             return data;
@@ -120,7 +160,7 @@ namespace ExcelManagement.ClassLibary
         //---------------------------------------------------------------------------//
 
         //New Row / get first row (assuming it has data)
-        public dynamic GetFirstRow(string workbookName, string sheetName, string directory)
+        public dynamic GetLastUnusedRow(string workbookName, string sheetName, string directory)
         {
             string fullFilePath = GetFilePath(directory, workbookName);
             var workbook = new XLWorkbook(fullFilePath);
@@ -156,11 +196,13 @@ namespace ExcelManagement.ClassLibary
             }
 
             var workbook = new XLWorkbook(fullFilePath);
-            var dataSheets = new Dictionary<string, List<dynamic>>();
+            var data = new Dictionary<string, List<dynamic>>();
 
             foreach (var worksheet in workbook.Worksheets)
             {
-                //IF Sheet empty header or data, add new data to header row, and save it.
+
+                //IF Sheet first row (heder row) has no data. add new data to header row, and save it.
+                //A sheet MUST have a header row
                 if (worksheet.FirstRow().CellsUsed().Count() == 0)
                 {
                     var maxCols = 4; // If the sheet doesn't contain any rows, add data to header.
@@ -173,13 +215,14 @@ namespace ExcelManagement.ClassLibary
                     workbook.SaveAs(fullFilePath);
                 }
 
-                // If the sheet contains data
-                var headers = worksheet.RowsUsed().First();
-                var dataRows = worksheet.RowsUsed().Skip(1);
-                var sheetData = new List<dynamic>();
-
+                //If Sheet has Header and data
                 if (worksheet.RowsUsed().Skip(1).Count() != 0)
                 {
+                    //here we assume that header row and atleast 1 other row has data.
+                    var headers = worksheet.RowsUsed().First();
+                    var dataRows = worksheet.RowsUsed().Skip(1);
+                    var sheetData = new List<dynamic>();
+
                     foreach (var dataRow in dataRows)
                     {
                         dynamic item = new ExpandoObject();
@@ -194,35 +237,114 @@ namespace ExcelManagement.ClassLibary
 
                         sheetData.Add(item);
                     }
-
+                    data[worksheet.Name] = sheetData;
                 }
-
-                else //if header colum has data, but rest dossent. then select first 4 rows
+                else // if sheet dossent have data
                 {
-                    int maxRows = 2; //how many empty rows sould be fetched
+                    int maxEmptyRowsToDisplay = 2;
+                    var headers = worksheet.RowsUsed().First();
+                    var sheetData = new List<dynamic>();
 
-                    for (int row = 2; row <= maxRows + 1; row++)
+                    for (int rowIndex = 2; rowIndex < maxEmptyRowsToDisplay + 2; rowIndex++)
                     {
                         dynamic item = new ExpandoObject();
                         var dictionary = (IDictionary<string, object>)item;
 
-                        dictionary["GridId"] = row;
-
-                        for (int i = 0; i < headers.Cells().Count(); i++) //Cells assign placeholder values
+                        dictionary["GridId"] = rowIndex;
+                        for (int columnIndex = 1; columnIndex < headers.Cells().Count() + 1; columnIndex++)
                         {
-                            var xlCellView = new XlCellView(worksheet.Cell(row, i + 1));
-                            dictionary[headers.Cell(i + 1).Value.ToString().Replace(" ", "")] = xlCellView;
+                            var xlCellView = new XlCellView(worksheet.Cell(rowIndex, columnIndex));
+                            dictionary[headers.Cell(columnIndex).Value.ToString().Replace(" ", "")] = xlCellView;
                         }
 
                         sheetData.Add(item);
                     }
+                    data[worksheet.Name] = sheetData;
                 }
 
-                dataSheets[worksheet.Name] = sheetData;
             }
-            KeyValuePair<string, Dictionary<string, List<dynamic>>> WorkbookData = new(workbookName, dataSheets);
 
-            return WorkbookData;
+            var workBookData = new KeyValuePair<string, Dictionary<string, List<dynamic>>>(workbookName, data);
+
+            return workBookData;
+        }
+
+        //Get Worksheet
+        public KeyValuePair<string, List<dynamic>> GetWorksheet(string workbookName, string sheetName, string directory)
+        {
+            string fullFilePath = GetFilePath(directory, workbookName);
+
+            if (!fileLogic.IsFileAccessable(fullFilePath))
+            {
+                throw new IOException();
+            }
+
+            var workbook = new XLWorkbook(fullFilePath);
+            var worksheet = workbook.Worksheet(sheetName);
+            var data = new KeyValuePair<string, List<dynamic>>();
+
+            //IF Sheet first row (heder row) has no data. add new data to header row, and save it.
+            //A sheet MUST have a header row
+            if (worksheet.FirstRow().CellsUsed().Count() == 0)
+            {
+                var maxCols = 4; // If the sheet doesn't contain any rows, add data to header.
+
+                for (int col = 1; col <= maxCols; col++) //Header assign placeholder value
+                {
+                    worksheet.Cell(1, col).Value = $"Column {col}";
+                }
+
+                workbook.SaveAs(fullFilePath);
+            }
+
+            //If Sheet has Header and data
+            if (worksheet.RowsUsed().Skip(1).Count() != 0)
+            {
+                //here we assume that header row and atleast 1 other row has data.
+                var headers = worksheet.RowsUsed().First();
+                var dataRows = worksheet.RowsUsed().Skip(1);
+                var sheetData = new List<dynamic>();
+
+                foreach (var dataRow in dataRows)
+                {
+                    dynamic item = new ExpandoObject();
+                    var dictionary = (IDictionary<string, object>)item;
+
+                    dictionary["GridId"] = dataRow.RowNumber();
+                    for (int i = 0; i < headers.Cells().Count(); i++)
+                    {
+                        var xlCellView = new XlCellView(dataRow.Cell(i + 1));
+                        dictionary[headers.Cell(i + 1).Value.ToString().Replace(" ", "")] = xlCellView;
+                    }
+
+                    sheetData.Add(item);
+                }
+                data = new KeyValuePair<string, List<dynamic>>(sheetName, sheetData);
+            }
+            else // if sheet dossent have data
+            {
+                int maxEmptyRowsToDisplay = 2;
+                var headers = worksheet.RowsUsed().First();
+                var sheetData = new List<dynamic>();
+
+                for (int rowIndex = 2; rowIndex < maxEmptyRowsToDisplay + 2; rowIndex++)
+                {
+                    dynamic item = new ExpandoObject();
+                    var dictionary = (IDictionary<string, object>)item;
+
+                    dictionary["GridId"] = rowIndex;
+                    for (int columnIndex = 1; columnIndex < headers.Cells().Count() + 1; columnIndex++)
+                    {
+                        var xlCellView = new XlCellView(worksheet.Cell(rowIndex, columnIndex));
+                        dictionary[headers.Cell(columnIndex).Value.ToString().Replace(" ", "")] = xlCellView;
+                    }
+
+                    sheetData.Add(item);
+                }
+                data = new KeyValuePair<string, List<dynamic>>(sheetName, sheetData);
+            }
+
+            return data;
         }
 
         //---------------------------------------------------------------------------//
@@ -236,6 +358,11 @@ namespace ExcelManagement.ClassLibary
             if (!fileLogic.IsFileAccessable(fullFilePath))
             {
                 throw new IOException();
+            }
+
+            using (var workbook = new XLWorkbook(fullFilePath))
+            {
+                workbook.SaveAs(fullFilePath);
             }
 
             using (var workbook = new XLWorkbook(fullFilePath))
@@ -261,6 +388,8 @@ namespace ExcelManagement.ClassLibary
 
                 workbook.Worksheets.Add(newSheetName);
                 workbook.SaveAs(fullFilePath);
+
+                int x = 3;
             }
         }
 
@@ -327,28 +456,30 @@ namespace ExcelManagement.ClassLibary
 
                 //If new item, find last alvalabe index that is empty
                 //this insure that 2 users cant overrite eachother
-                if (rowIndex == 0)
+                if (rowIndex == 0) //If new row
                 {
                     rowIndex = worksheet.LastRowUsed().RowNumber() + 1;
                 }
-
-                //Duplicate Data Counter
-                int NewCellData = 0;
-                foreach (var cellObject in ((IDictionary<string, object>)updatedDataItem).Values.Skip(1))
+                else //If not new row
                 {
-                    var cell = (XlCellView)cellObject;
-                    int columnIndex = cell.XlCell.Address.ColumnNumber;
-
-                    if (worksheet.Cell(rowIndex, columnIndex).Value.ToString() != cell.Value.ToString()) //all new = old?
+                    //Duplicate Data Counter
+                    int NewCellData = 0;
+                    foreach (var cellObject in ((IDictionary<string, object>)updatedDataItem).Values.Skip(1))
                     {
-                        NewCellData++;
-                    }
-                }
+                        var cell = (XlCellView)cellObject;
+                        int columnIndex = cell.XlCell.Address.ColumnNumber;
 
-                //If Duplicate
-                if (NewCellData == 0)
-                {
-                    throw new DuplicateRowDataException("");
+                        if (worksheet.Cell(rowIndex, columnIndex).Value.ToString() != cell.Value.ToString()) //all new = old?
+                        {
+                            NewCellData++;
+                        }
+                    }
+
+                    //If Duplicate
+                    if (NewCellData == 0)
+                    {
+                        throw new DuplicateRowDataException("");
+                    }
                 }
 
                 //Add new Data to cells
@@ -410,8 +541,8 @@ namespace ExcelManagement.ClassLibary
 
             using (var workbook = new XLWorkbook(fullFilePath))
             {
-                //var worksheet = workbook.Worksheet(sheetName);
-                //worksheet.Row(rowIndex).Delete();
+                var worksheet = workbook.Worksheet(sheetName);
+                worksheet.Row(rowIndex).Delete();
 
                 workbook.SaveAs(fullFilePath);
             }
---
+++ ExcelManagement/ExcelManagement.Testing/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ExcelManagement.Testing
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
