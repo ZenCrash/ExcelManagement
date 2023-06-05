3ba8f05fca97a1620ff72a4f78862b87082af5a0
Allan <76095439+ZenCrash@users.noreply.github.com>
2023-06-05 14:45:38 +0200

    login initially.

---
+++ ExcelManagement/ExcelManagement.DxBlazor/Pages/Account/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExcelManagement.DxBlazor.Pages.Account
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
