a1f336025e826629d1587fd5396a9b51e45030e4
Allan <76095439+ZenCrash@users.noreply.github.com>
2023-07-03 13:09:50 +0200

    scripts updated.

--- ExcelManagement/BluestarDatabase/Database.Create.TestData.sql
+++ ExcelManagement/BluestarDatabase/Database.Create.TestData.sql
@@ -1,27 +1,50 @@
-﻿/* Create Test Data Locally */
+﻿/* CREATE BluestarDatabase DATABASE */
 
-USE [BluestarDatabase]
+IF EXISTS (SELECT * FROM sys.databases WHERE name = 'BluestarDatabase')
+	BEGIN
+		/* CREATE TABELS */
+		USE [BluestarDatabase]
+
+		--local variables
+		SET @CompanyId1 = NEWID();
+		SET @CompanyId2 = NEWID();
+
+		--insert Companys
+		insert into Companies ([CompanyId], [CompanyName], [Description], [CompanyLogoUrl], [CreatedDate], [UpdatedDate]) 
+		values (NEWID(), 'Intersteller Think Tank',	'Intersteller Think Tank - DescriptionText', 'www.google.com', GETDATE(), GETDATE());
+		insert into Companies ([CompanyId], [CompanyName], [Description], [CompanyLogoUrl], [CreatedDate], [UpdatedDate]) 
+		values (NEWID(), 'Dog Fight International',	'Dog Fight International - DescriptionText', 'www.google.com', GETDATE(), GETDATE());
+
+		BEGIN
+			insert into Companies ([CompanyId], [CompanyName], [Description], [CompanyLogoUrl], [CreatedDate], [UpdatedDate])
+			values (NEWID(), 'pdm technology Group', 'pdm technology Group - DescriptionText', 'www.google.com', GETDATE(), GETDATE());
+		END
+		BEGIN
+			insert into Companies ([CompanyId], [CompanyName], [Description], [CompanyLogoUrl], [CreatedDate], [UpdatedDate]) 
+			values (NEWID(), 'Alvon Dynamics', 'Alvon Dynamics - DescriptionText', 'www.google.com', GETDATE(), GETDATE());
+			SET @CompanyId2 = SCOPE_IDENTITY()
+		END
+
+		--insert Groups
+		BEGIN
+			insert into Groups ([GroupId], [GroupName], [Description], [GroupLogoUrl], [CompanyId], [CreatedDate], [UpdatedDate]) 
+			values (NEWID(), 'pdm technology Group - IT Dep', 'pdm technology Group - IT Dep - Description', 'www.google.com', @CompanyId1, GETDATE(), GETDATE());
+			insert into Groups ([GroupId], [GroupName], [Description], [GroupLogoUrl], [CompanyId], [CreatedDate], [UpdatedDate]) 
+			values (NEWID(), 'pdm technology Group - HR Dep', 'pdm technology Group - HR Dep - Description', 'www.google.com', @CompanyId1, GETDATE(), GETDATE());
+			insert into Groups ([GroupId], [GroupName], [Description], [GroupLogoUrl], [CompanyId], [CreatedDate], [UpdatedDate]) 
+			values (NEWID(), 'pdm technology Group - Media Dep', 'pdm technology Group - Media Dep - Description', 'www.google.com', @CompanyId1, GETDATE(), GETDATE());
+			insert into Groups ([GroupId], [GroupName], [Description], [GroupLogoUrl], [CompanyId], [CreatedDate], [UpdatedDate]) 
+			values (NEWID(), 'pdm technology Group - Exe Dep', 'pdm technology Group - Exe Dep - Description', 'www.google.com', @CompanyId1, GETDATE(), GETDATE());;
+		END
+		BEGIN
+			insert into Groups ([GroupId], [GroupName], [Description], [GroupLogoUrl], [CompanyId], [CreatedDate], [UpdatedDate]) 
+			values (NEWID(), 'Alvon Dynamics - IT Dep', 'Alvon Dynamics - IT Dep - Description', 'www.google.com', @CompanyId2, GETDATE(), GETDATE());
+			insert into Groups ([GroupId], [GroupName], [Description], [GroupLogoUrl], [CompanyId], [CreatedDate], [UpdatedDate]) 
+			values (NEWID(), 'Alvon Dynamics - HR Dep', 'Alvon Dynamics - HR Dep - Description', 'www.google.com', @CompanyId2, GETDATE(), GETDATE());
+			insert into Groups ([GroupId], [GroupName], [Description], [GroupLogoUrl], [CompanyId], [CreatedDate], [UpdatedDate]) 
+			values (NEWID(), 'Alvon Dynamics - Media Dep', 'Alvon Dynamics - Media Dep - Description', 'www.google.com', @CompanyId2, GETDATE(), GETDATE());
+			insert into Groups ([GroupId], [GroupName], [Description], [GroupLogoUrl], [CompanyId], [CreatedDate], [UpdatedDate]) 
+			values (NEWID(), 'Alvon Dynamics - Exe Dep',	'Alvon Dynamics - Exe Dep - Description', 'www.google.com', @CompanyId2, GETDATE(), GETDATE());
+		END
+	END;
 GO
-
---Companies
-insert into Companies ([Id], [CompanyName], [Description], [CompanyLogoUrl]) values (NEWID(), 'pdm technology Group',		'pdm technology Group - DescriptionText',		'www.google.com');
-insert into Companies ([Id], [CompanyName], [Description], [CompanyLogoUrl]) values (NEWID(), 'Alvon Dynamics',			'Alvon Dynamics - DescriptionText',				'www.google.com');
-insert into Companies ([Id], [CompanyName], [Description], [CompanyLogoUrl]) values (NEWID(), 'Intersteller Think Tank',	'Intersteller Think Tank - DescriptionText',	'www.google.com');
-insert into Companies ([Id], [CompanyName], [Description], [CompanyLogoUrl]) values (NEWID(), 'Dog Fight International',	'Dog Fight International - DescriptionText',	'www.google.com');
-
-GO
-
---USE [BluestarDatabase]
---GO
-----Departments
---insert into Departments ([Id], [DepartmentName], [Description], [CompanyId]) values (NEWID(), 'pdm technology Group - IT Dep',		'pdm technology Group - IT Dep - Description',		'9BB04FE6-87E2-48A9-8914-D6B44F97F326');
---insert into Departments ([Id], [DepartmentName], [Description], [CompanyId]) values (NEWID(), 'pdm technology Group - HR Dep',		'pdm technology Group - HR Dep - Description',		'9BB04FE6-87E2-48A9-8914-D6B44F97F326');
---insert into Departments ([Id], [DepartmentName], [Description], [CompanyId]) values (NEWID(), 'pdm technology Group - Media Dep',	'pdm technology Group - Media Dep - Description',	'9BB04FE6-87E2-48A9-8914-D6B44F97F326');
---insert into Departments ([Id], [DepartmentName], [Description], [CompanyId]) values (NEWID(), 'pdm technology Group - Exe Dep',	'pdm technology Group - Exe Dep - Description',		'9BB04FE6-87E2-48A9-8914-D6B44F97F326');
-
---insert into Departments ([Id], [DepartmentName], [Description], [CompanyId]) values (NEWID(), 'Alvon Dynamics - IT Dep',			'Alvon Dynamics - IT Dep - Description',			'06278E21-C621-4818-B636-BE73CA239AC4');
---insert into Departments ([Id], [DepartmentName], [Description], [CompanyId]) values (NEWID(), 'Alvon Dynamics - HR Dep',			'Alvon Dynamics - HR Dep - Description',			'06278E21-C621-4818-B636-BE73CA239AC4');
---insert into Departments ([Id], [DepartmentName], [Description], [CompanyId]) values (NEWID(), 'Alvon Dynamics - Media Dep',		'Alvon Dynamics - Media Dep - Description',			'06278E21-C621-4818-B636-BE73CA239AC4');
---insert into Departments ([Id], [DepartmentName], [Description], [CompanyId]) values (NEWID(), 'Alvon Dynamics - Exe Dep',			'Alvon Dynamics - Exe Dep - Description',			'06278E21-C621-4818-B636-BE73CA239AC4');
-
---GO
