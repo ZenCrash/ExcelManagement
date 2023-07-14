f59df213251d96d30a64ebac46e186dcd21817e2
Allan <76095439+ZenCrash@users.noreply.github.com>
2023-07-14 17:31:57 +0200

    incomplete updated tables

---
+++ ExcelManagement/BluestarDatabase/Database.Create.Tables.New.sql
@@ -0,0 +1,128 @@
+﻿Create Database [BluestarDatabase]
+GO
+Use [BluestarDatabase]
+GO
+
+CREATE TABLE Company(
+	[Id] [INT] IDENTITY(1,1) NOT NULL,
+	[CompanyName] [nvarchar](2000) NOT NULL,
+	[Description] [nvarchar](4000) NULL,
+	[CompanyLogoUrl] [nvarchar](4000) NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[UpdatedDate] [datetime] NOT NULL
+
+	PRIMARY KEY(Id)
+);
+GO
+
+CREATE TABLE People(
+	Id INT IDENTITY(1,1) NOT NULL,
+
+	Created_By INT NOT NULL,
+	Update_By INT NOT NULL,
+	Company_Id INT NOT NULL,
+
+	PRIMARY KEY(Id),
+	FOREIGN KEY(Created_By) REFERENCES People(Id),
+	FOREIGN KEY(Update_By) REFERENCES People(Id),
+	FOREIGN KEY(Company_Id) REFERENCES Company(Id)
+);
+GO
+
+CREATE TABLE Roles(
+	Id INT IDENTITY(1,1) NOT NULL,
+
+	Created_By INT NOT NULL,
+	Update_By INT NOT NULL,
+	Company_Id INT NOT NULL,
+
+	PRIMARY KEY(Id),
+	FOREIGN KEY(Created_By) REFERENCES People(Id),
+	FOREIGN KEY(Update_By) REFERENCES People(Id),
+	FOREIGN KEY(Company_Id) REFERENCES Company(Id)
+);
+GO
+
+CREATE TABLE Groups(
+	Id INT IDENTITY(1,1) NOT NULL,
+
+	Created_By INT NOT NULL,
+	Update_By INT NOT NULL,
+	Company_Id INT NOT NULL,
+
+	PRIMARY KEY(Id),
+	FOREIGN KEY(Created_By) REFERENCES People(Id),
+	FOREIGN KEY(Update_By) REFERENCES People(Id),
+	FOREIGN KEY(Company_Id) REFERENCES Company(Id)
+);
+GO
+
+CREATE TABLE FileAndFolders(
+	Id INT IDENTITY(1,1) NOT NULL,
+
+	Created_By INT NOT NULL,
+	Update_By INT NOT NULL,
+	Company_Id INT NOT NULL,
+
+	PRIMARY KEY(Id),
+	FOREIGN KEY(Created_By) REFERENCES People(Id),
+	FOREIGN KEY(Update_By) REFERENCES People(Id),
+	FOREIGN KEY(Company_Id) REFERENCES Company(Id)
+);
+GO
+
+Create Table GroupingGroups (
+	Parent_Group_Id Int NOT NULL,
+	Child_Group_Id INT NOT NULL,
+
+	PRIMARY KEY(Parent_Group_Id, Child_Group_Id),
+	FOREIGN KEY(Parent_Group_Id) REFERENCES Groups(Id),
+	FOREIGN KEY(Child_Group_Id) REFERENCES Groups(Id)
+);
+GO
+
+CREATE TABLE PeopleRoles(
+	Person_Id INT NOT NULL,
+	Role_Id INT NOT NULL,
+
+	PRIMARY KEY(Person_Id, Role_Id),
+	FOREIGN KEY(Person_Id) REFERENCES People(Id),
+	FOREIGN KEY(Role_Id) REFERENCES Roles(Id)
+);
+GO
+
+CREATE TABLE PeopleGroup(
+	Person_Id INT NOT NULL,
+	Group_Id INT NOT NULL,
+	PRIMARY KEY(Person_Id, Group_Id),
+	FOREIGN KEY(Person_Id) REFERENCES People(Id),
+	FOREIGN KEY(Group_Id) REFERENCES Groups(Id)
+);
+GO
+
+CREATE TABLE FileAndFolderGroup(
+	Group_Id INT NOT NULL,
+	file_Id INT NOT NULL,
+	PRIMARY KEY(Group_Id, file_Id),
+	FOREIGN KEY(Group_Id) REFERENCES Groups(Id),
+	FOREIGN KEY(file_Id) REFERENCES FileAndFolders(Id)
+);
+GO
+
+CREATE TABLE FileAndFolderPeople(
+	Person_Id INT NOT NULL,
+	File_Id INT NOT NULL,
+	PRIMARY KEY(Person_Id, File_Id),
+	FOREIGN KEY(Person_Id) REFERENCES People(Id),
+	FOREIGN KEY(File_Id) REFERENCES FileAndFolders(Id)
+);
+GO
+
+CREATE TABLE FileAndFolderRoles(
+	Role_Id INT NOT NULL,
+	file_Id INT NOT NULL,
+	PRIMARY KEY(Role_Id, file_Id),
+	FOREIGN KEY(Role_Id) REFERENCES Roles(Id),
+	FOREIGN KEY(file_Id) REFERENCES FileAndFolders(Id)
+);
+GO
---
+++ ExcelManagement/BluestarDatabase/Database.Create.Tables.sql
@@ -0,0 +1,141 @@
+﻿/* Careate Tables */
+CREATE DATABASE [ExcelManagement]
+GO
+
+USE [ExcelManagement]
+GO
+
+--Tables
+CREATE TABLE [dbo].[companies](
+	[Id] [UNIQUEIDENTIFIER] PRIMARY KEY NOT NULL,
+	[CompanyName] [nvarchar](2000) NOT NULL,
+	[Description] [nvarchar](4000) NULL,
+	[CompanyLogoUrl] [nvarchar](4000) NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[UpdatedDate] [datetime] NOT NULL
+)
+GO
+
+CREATE TABLE [dbo].[people](
+	[Id] [nvarchar](450) PRIMARY KEY NOT NULL,
+	[FirstName] [nvarchar](256) NOT NULL,
+	[LastName] [nvarchar](256) NOT NULL,
+	[JobTitle] [nvarchar](256) NULL,
+	[Bio] [nvarchar](4000) NULL,
+	[ProfileImageUrl] [nvarchar](4000) NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[UpdatedDate] [datetime] NOT NULL,
+
+	--Relationships
+	[CompanyId] [UNIQUEIDENTIFIER] NOT NULL,
+	[CreatedById] [nvarchar](450) NULL,
+	[UpdatedById] [nvarchar](450) NULL,
+
+	CONSTRAINT [FK_people_company_CompanyId] FOREIGN KEY ([CompanyId]) REFERENCES [companies] (Id) ON DELETE CASCADE,
+	CONSTRAINT [FK_people_people_CreatedById] FOREIGN KEY ([CreatedById]) REFERENCES [people] (Id) ON DELETE SET NULL,
+	CONSTRAINT [FK_people_people_UpdatedById] FOREIGN KEY ([UpdatedById]) REFERENCES [people] (Id) ON DELETE SET NULL
+)
+GO
+
+CREATE TABLE [dbo].[roles](
+	[Id] [nvarchar](450) PRIMARY KEY NOT NULL,
+	[Description] [nvarchar](4000) NULL,
+	[RoleLogoUrl] [nvarchar](4000) NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[UpdatedDate] [datetime] NOT NULL,
+
+	--Relationships
+	[CompanyId] [UNIQUEIDENTIFIER] NOT NULL,
+	[CreatedById] [nvarchar](450) NULL,
+	[UpdatedById] [nvarchar](450) NULL,
+
+	CONSTRAINT [FK_people_company_CompanyId] FOREIGN KEY ([CompanyId]) REFERENCES [companies] (Id) ON DELETE CASCADE,
+	CONSTRAINT [FK_people_people_CreatedById] FOREIGN KEY ([CreatedById]) REFERENCES [people] (Id) ON DELETE SET NULL,
+	CONSTRAINT [FK_people_people_UpdatedById] FOREIGN KEY ([UpdatedById]) REFERENCES [people] (Id) ON DELETE SET NULL
+)
+GO
+
+CREATE TABLE [dbo].[groups](
+	[Id] [UNIQUEIDENTIFIER] PRIMARY KEY NOT NULL,
+	[GroupName] [nvarchar](512) NOT NULL,
+	[Description] [nvarchar](4000) NULL,
+	[GroupLogoUrl] [nvarchar](4000) NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[UpdatedDate] [datetime] NOT NULL,
+
+	--Relationships
+	[CompanyId] [UNIQUEIDENTIFIER] NOT NULL,
+	[CreatedById] [nvarchar](450) NULL,
+	[UpdatedById] [nvarchar](450) NULL,
+
+	CONSTRAINT [FK_groups_company_CompanyId] FOREIGN KEY ([CompanyId]) REFERENCES [companies] (Id) ON DELETE CASCADE,
+	CONSTRAINT [FK_groups_people_CreatedById] FOREIGN KEY ([CreatedById]) REFERENCES [people] (Id) ON DELETE SET NULL,
+	CONSTRAINT [FK_groups_people_UpdatedById] FOREIGN KEY ([UpdatedById]) REFERENCES [people] (Id) ON DELETE SET NULL
+)
+GO
+
+CREATE TABLE [dbo].[files_and_folders](
+	[Id] [UNIQUEIDENTIFIER] PRIMARY KEY NOT NULL,
+	[ItemName] [nvarchar](512) NOT NULL,
+	[Description] [nvarchar](4000) NULL,
+	[RelativeFilePath] [nvarchar](4000) NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[UpdatedDate] [datetime] NOT NULL,
+	[DataType] [nvarchar] NOT NULL,
+
+	--Relationships
+	[CompanyId] [UNIQUEIDENTIFIER] NOT NULL,
+	[CreatedById] [nvarchar](450) NULL,
+	[UpdatedById] [nvarchar](450) NULL,
+
+	CONSTRAINT [FK_files_and_folders_company_CompanyId] FOREIGN KEY ([CompanyId]) REFERENCES [companies] (Id) ON DELETE CASCADE,
+	CONSTRAINT [FK_files_and_folders_people_CreatedById] FOREIGN KEY ([CreatedById]) REFERENCES [people] (Id) ON DELETE SET NULL,
+	CONSTRAINT [FK_files_and_folders_people_UpdatedById] FOREIGN KEY ([UpdatedById]) REFERENCES [people] (Id) ON DELETE SET NULL
+)
+GO
+
+--Brigde Tables
+CREATE TABLE [people_roles] ( -- People + Roles
+	[PersonId] [nvarchar](450) NOT NULL,
+	[RoleId] [nvarchar](450) NOT NULL,
+	CONSTRAINT [PK_people_roles_Id] PRIMARY KEY ([PersonId], [RoleId]),
+	CONSTRAINT [FK_people_roles_people_PersonId] FOREIGN KEY ([PersonId]) REFERENCES [people] (Id) ON DELETE CASCADE,
+	CONSTRAINT [FK_people_roles_roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [roles] (Id) ON DELETE CASCADE
+)
+GO
+
+CREATE TABLE [people_groups] ( -- People + Groups
+	[PersonId] [nvarchar](450) NOT NULL,
+	[GroupId] [UNIQUEIDENTIFIER] NOT NULL,
+	CONSTRAINT [PK_people_groups_Id] PRIMARY KEY ([PersonId], [GroupId]),
+	CONSTRAINT [FK_people_groups_people_PersonId] FOREIGN KEY ([PersonId]) REFERENCES [people] (Id) ON DELETE CASCADE,
+	CONSTRAINT [FK_people_groups_groups_GroupId] FOREIGN KEY ([GroupId]) REFERENCES [groups] (Id) ON DELETE CASCADE
+)
+GO
+
+CREATE TABLE [people_files_and_folders] ( -- People + Files_And_Folders
+	[PersonId] [nvarchar](450) NOT NULL,
+	[FileAndFolderId] [UNIQUEIDENTIFIER] NOT NULL,
+	CONSTRAINT [PK_people_files_and_folders_Id] PRIMARY KEY ([PersonId], [FileAndFolderId]),
+	CONSTRAINT [FK_people_files_and_folders_people_PersonId] FOREIGN KEY ([PersonId]) REFERENCES [people] (Id) ON DELETE CASCADE,
+	CONSTRAINT [FK_people_files_and_folders_files_and_folders_FileAndFolderId] FOREIGN KEY ([FileAndFolderId]) REFERENCES [files_and_folders] (Id) ON DELETE CASCADE
+)
+GO
+
+CREATE TABLE [roles_files_and_folders] ( -- Roles + Files_And_Folder
+	[RoleId] [nvarchar](450) NOT NULL,
+	[FileAndFolderId] [UNIQUEIDENTIFIER] NOT NULL,
+	CONSTRAINT [PK_roles_files_and_folders_Id] PRIMARY KEY ([RoleId], [FileAndFolderId]),
+	CONSTRAINT [FK_roles_files_and_folders_roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [roles] (Id) ON DELETE CASCADE,
+	CONSTRAINT [FK_roles_files_and_folders_files_and_folders_FileAndFolderId] FOREIGN KEY ([FileAndFolderId]) REFERENCES [files_and_folders] (Id) ON DELETE CASCADE
+)
+GO
+
+CREATE TABLE [groups_files_and_folders] ( -- Groups + Files_And_Folders
+	[GroupId] [UNIQUEIDENTIFIER] NOT NULL,
+	[FileAndFolderId] [UNIQUEIDENTIFIER] NOT NULL,
+	CONSTRAINT [PK_groups_files_and_folders_Id] PRIMARY KEY ([GroupId], [FileAndFolderId]),
+	CONSTRAINT [FK_groups_files_and_folders_groups_GroupId] FOREIGN KEY ([GroupId]) REFERENCES [groups] (Id) ON DELETE CASCADE,
+	CONSTRAINT [FK_groups_files_and_folders_files_and_folders_FileAndFolderId] FOREIGN KEY ([FileAndFolderId]) REFERENCES [files_and_folders] (Id) ON DELETE CASCADE
+)
+GO
